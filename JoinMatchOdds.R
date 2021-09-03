63c3690b1b51a72e72214989bb3c613ab3049006
ewen_ <ewenhenderson@gmail.com>
2021-09-03 16:01:09 +0100

    reverse names better performance

--- JoinMatchOdds.R
+++ JoinMatchOdds.R
@@ -19,16 +19,40 @@ odds$Date <- as.Date(odds$Date)
 # names -------------------------------------------------------------------
 
 matches_names <- tibble(match_name = unique(c(matches$player_i, matches$player_j)))
+matches_names$match_name_rev <- sapply(
+  strsplit(matches_names$match_name, "\\s+"), function(x) paste(rev(x), collapse=" ")
+  )
+
 odds_names <- tibble(odds_name = unique(c(odds$Winner, odds$Loser)))
 
 # fuzzy match
 matched_names <- stringdist_left_join(
-  matches_names, odds_names, by = c("match_name"="odds_name"),
+  matches_names, odds_names, by = c("match_name_rev"="odds_name"),
   method = "jw", distance_col = "dist"
   )
 
 best_matches <- matched_names %>% 
   group_by(match_name) %>% 
-  arrange(match_name,dist)
+  dplyr::filter(dist == min(dist))
 
 # join --------------------------------------------------------------------
+
+matches_odds <- matches %>% 
+  # join name lookup
+  left_join(
+    select(best_matches, match_name, odds_name),
+    by = c("winner_name"="match_name")
+    ) %>% 
+  left_join(
+    select(best_matches, match_name, odds_name),
+    by = c("loser_name"="match_name")
+  ) %>% 
+  # join odds data
+  left_join(
+    odds, by = c(
+      "tourney_date"="Date",
+      "odds_name.x"="Winner",
+      "odds_name.y"="Loser"
+    )
+  )
+
